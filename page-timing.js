72ca3454fc3db53c2c1981b087c7557330a35e6f
Ananya <ananya.neogi@box8.in>
2018-11-27 02:41:28 +0530

    navigation timing api

---
+++ page-timing.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://oodle-demo.firebaseapp.com/#/browse');
+
+    const performanceTiming = JSON.parse(
+        await page.evaluate(() => JSON.stringify(window.performance.timing))
+    );
+
+    const timings = {};
+    Object.keys(performanceTiming).map((key, index) => {
+            timings[key] = performanceTiming[key] - performanceTiming.navigationStart;
+    });
+    console.log('performanceTiming', performanceTiming)
+    console.log('difference with navigation start', timings);
+
+    await browser.close();
+})();
